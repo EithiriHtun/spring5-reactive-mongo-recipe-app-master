bb34b07decd2c0d6e10d0e30e1a4fb5efbfc567f
eithiri <eithiritun1997@gmail.com>
2020-06-09 16:10:53 +0630

    Reactive Repositories #331

--- src/main/java/guru/springframework/repositories/recative/UnitOfMeasureReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/recative/UnitOfMeasureReactiveRepository.java
@@ -1,4 +1,8 @@
 package guru.springframework.repositories.recative;
 
-public interface UnitOfMeasureReactiveRepository {
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure,String> {
+
 }
